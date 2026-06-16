58341c063b26253f217f12792b53049685a1a2c7
root <root@devops-ubuntu.us-central1-a.c.devops-499515.internal>
2026-06-16 14:28:01 +0000

    mod script

--- Scripts/Get-DiskUsage.ps1
+++ Scripts/Get-DiskUsage.ps1
@@ -2,3 +2,9 @@ Get-PSDrive -PSProvider FileSystem |
 Select-Object Name,
 @{Name='UsedGB';Expression={[math]::Round(($_.Used/1GB),2)}},
 @{Name='FreeGB';Expression={[math]::Round(($_.Free/1GB),2)}}
+
+
+
+
+#pagefile
+ Get-WmiObject Win32_PageFileusage 
