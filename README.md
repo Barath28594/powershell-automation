49024ffe76d497f0e13564493b974b9068a7a8a0
root <root@devops-ubuntu.us-central1-a.c.devops-499515.internal>
2026-06-16 14:02:23 +0000

    Added disk usage script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # powershell-automation
+# Powershell Automation
+
+Infrastructure automation scripts.
+
+## Scripts
+
+- Get-DiskUsage.ps1
---
+++ Scripts/Get-DiskUsage.ps1
@@ -0,0 +1,4 @@
+Get-PSDrive -PSProvider FileSystem |
+Select-Object Name,
+@{Name='UsedGB';Expression={[math]::Round(($_.Used/1GB),2)}},
+@{Name='FreeGB';Expression={[math]::Round(($_.Free/1GB),2)}}
